8d0a341170f72fa051a8c50adf18a7559a003cfb
Ventsislav Georgiev <ventsislav.georgiev@live.com>
2014-11-18 17:03:38 +0200

    New List View

--- Streamus/Streamus.Web/Controllers/CollectionsController.cs
+++ Streamus/Streamus.Web/Controllers/CollectionsController.cs
@@ -1,9 +1,12 @@
 ﻿namespace Streamus.Web.Controllers
 {
 	using AutoMapper.QueryableExtensions;
+	using Kendo.Mvc.Extensions;
+	using Kendo.Mvc.UI;
 	using Streamus.Data;
 	using Streamus.Web.InputModels;
 	using Streamus.Web.ViewModels.Shared;
+	using System.Collections.Generic;
 	using System.Web.Mvc;
 
 	public class CollectionsController : BaseController
@@ -23,6 +26,22 @@
 			};
 
 			return PartialView("_Add", model);
+		}
+
+		[ChildActionOnly]
+		public ActionResult List()
+		{
+			return PartialView("_CollectionsList", this.GetData());
+		}
+
+		public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+		{
+			return Json(this.GetData().ToDataSourceResult(request));
+		}
+
+		private IEnumerable<CollectionViewModel> GetData()
+		{
+			return this.Data.Collections.All().Project().To<CollectionViewModel>();
 		}
 	}
 }
